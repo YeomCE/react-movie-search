eb60a12f7cd1a440b58f930af542198c40ee753e
yeomchaeeun <codms0654@gmail.com>
2023-06-16 02:00:10 +0900

    Movie-Search 2023-06-16 02:22

--- src/component/Banner.js
+++ src/component/Banner.js
@@ -1,8 +1,22 @@
 import React from 'react'
+import { useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import { movieAction } from '../redux/actions/movieAction';
 
 const Banner = (movie) => {
+
+    const navigate = useNavigate();
+    const dispatch = useDispatch();
+
+    const showDetail =()=>{
+        navigate(`/movies/${movie.id}`)
+        dispatch(movieAction.getDetailMovie(movie.id))
+        dispatch({type:"STATE_RESET"})
+    }
+
+    console.log(movie)
     return (
-        <div className='banner'>
+        <div className='banner' onClick={showDetail}>
             <img src={`https://www.themoviedb.org/t/p/w1920_and_h800_multi_faces/${movie.movie.poster_path}`} />
             <div className='movie-info'>
                 <h1 className='banner-title'>{movie.movie.title}</h1>
